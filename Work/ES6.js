f277bb5fbe8214695a5c188845bce7ae4c0261c8
Rosal-1998 <623723808@qq.com>
2024-08-17 16:15:20 +0800

    update

---
+++ Work/ES6.js
@@ -0,0 +1,10 @@
+let number = {}
+number.type = 'person'
+let fakerNumber = number
+fakerNumber.type = 'car'
+console.log(number)
+console.log(fakerNumber)
+
+number = {}
+console.log(number)
+console.log(fakerNumber)
